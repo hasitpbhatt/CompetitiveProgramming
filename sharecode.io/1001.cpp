c3d560ea00e739b32be120691d17d7ebe1e474c0
Hasit Bhatt <hasit.p.bhatt@gmail.com>
2015-07-04 10:18:36 +0530

    Initial Commit

---
+++ sharecode.io/1001.cpp
@@ -0,0 +1,11 @@
+// https://sharecode.io/section/problemset/problem/1001
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	long long a,b;
+	while(cin >> a >> b)
+	cout << a+b << endl;
+	return 0;
+}
